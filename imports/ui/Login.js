8198b7b191fc390f6adcbfa7ad52e8f014d89a0e
Xavok <xavoking@gmail.com>
2017-04-08 01:22:09 -0700

    Update login form language

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -42,7 +42,7 @@ export default class Login extends Component {
                         <input type="password" ref="password" name="password" placeholder="Password"/>
                         <button className="button">Login</button>
                     </form>
-                    <Link to="/signup">Have an Account?</Link>
+                    <Link to="/signup">Need an Account?</Link>
                 </div>
             </div>
         );
